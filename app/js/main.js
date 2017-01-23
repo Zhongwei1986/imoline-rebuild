b93e4173abba4d44acd5108b2d10c6ef8b091327
Zhongwei Zhou <zhouzhongwei1986@sina.com>
2017-01-23 20:37:58 +0800

    重构JS完成

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,26 @@
-$(function() {   
+$(function() {
     var FADE_TIME = 150; // ms
     var TYPING_TIMER_LENGTH = 400; // ms
-    var COLORS = [ 
+    var COLORS = [
         '#e21400', '#91580f', '#f8a700', '#f78b00',
         '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
         '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
     ];
-    
+
     //DOM节点
-    var $window = $(window); 
-    var $usernameInput = $('.usernameInput'); 
-    var $sysInfo = $('.sysInfo');
-    var $messages = $('.messages'); 
+    var $window = $(window);
+    var $usernameInput = $('.usernameInput');
+    var $sysInfos = $('.sysInfos');
+    var $numUsers = $('#numUsers');
+    var $userLists = $('.userLists');
+    var $messages = $('.messages');
     var $inputMessage = $('.inputMessage');
-    var $loginPage = $('.login.page'); 
-    var $chatPage = $('.chat.page'); 
-    
+    var $loginPage = $('.login.page');
+    var $chatPage = $('.chat.page');
+    var $clearBtn = $('.clearBtn');
+
     //全局变量
-    var clientName; 
+    var clientName;
     var connected = false;
     var typing = false;
     var lastTypingTime;
@@ -25,13 +28,13 @@ $(function() {
 
     //创建socket.io的客户端socket对象  
     var socket = io();
-    
-    /*登录相关*/    
+
+    /*登录相关*/
     //添加用户  
-    function addUser() {
+    function checkUsername() {
         clientName = cleanInput($usernameInput.val().trim());
         if (clientName) {
-            socket.emit('add user', clientName);
+            socket.emit('check username', clientName);
         }
     }
 
@@ -50,111 +53,129 @@ $(function() {
 
     //登录成功
     function loginSuccess() {
-        if (clientName) {
-            $loginPage.fadeOut(); 
-            $chatPage.show(); 
-            $loginPage.off('click'); 
-            $currentInput = $inputMessage.focus();           
-        }
+        $loginPage.fadeOut();
+        $chatPage.show();
+        $loginPage.off('click');
+        $currentInput = $inputMessage.focus();
     }
 
     /*DOM操作*/
-    //聊天框内容转移，避免注入攻击
+    //聊天框内容转义，避免注入攻击
     function cleanInput(input) {
         return $('<div/>').html(input).text();
-    }    
+    }
 
     //添加系统信息
-    function addSysInfo(data) {       
-        var $sysElement = $('<li>').addClass('log').css('color','yellow'); 
-        if (data.msg) {            
-            $sysElement.text('系统:  ' + data.msg);
-            $sysInfo.append($sysElement);
+    function addSysInfo(msg) {
+        var $sysInfoEl = $('<li>').addClass('sysInfo').css('color', 'red');
+        if (msg) {
+            $sysInfoEl.text('系统:  ' + msg);
+            $sysInfos.append($sysInfoEl);
         }
-    }    
+        $sysInfos[0].scrollTop = $sysInfos[0].scrollHeight;
+    }
 
-    //增加聊天信息节点
+    //增加聊天区域显示内容
     function addChatMessage(data) {
-        var $typingMessages = getTypingMessages(data);
-        var fade = true; 
+        var $typingMessages = getTypingMessages(data.from);
+        var fade = true;
         if ($typingMessages.length !== 0) {
             fade = false;
-            $typingMessages.remove();
+            $typingMessages.remove()
         }
 
-        var typingClass = data.typing ? typing : '' ;        
+        var typingClass = data.typing ? 'typing' : '';
         var $usernameDiv = $('<span class="username"/>').text(data.from + ": ")
-                                .css('color', getUsernameColor(data.from)); 
-        var $messageBodyDiv = $('<span class="messageBody">').text(data.msg); 
-        var $messageDiv = $('<li class="message"/>').data('username', data.from);        
-       
+            .css('color', getUsernameColor(data.from));
+        var $messageBodyDiv = $('<span class="messageBody">').text(data.msg);
+        var $messageDiv = $('<li class="message"/>').data('username', data.from);
+
         if (data.from !== clientName) { //非本人发出信息
             if (data.mode == 'private') { //其它用户私聊
-                $usernameDiv.dbclick(function() {
-                        $inputMessage.val('@' + data.from + ': ' + $inputMessage.val()).focus();
-                    });
+                $usernameDiv.click(function() {
+                    $inputMessage.val('@' + data.from + ': ' + $inputMessage.val()).focus();
+                });
                 $messageBodyDiv.css('color', 'blue');
             } else if (data.mode == 'public') { //公聊
                 if (typingClass) {
-
+                    $messageDiv.addClass(typingClass);
                 } else {
-
+                    $usernameDiv.dblclick(function() {
+                        $inputMessage.val('@' + data.from + ': ' + $inputMessage.val()).focus();
+                    });
                 }
-                $usernameDiv.dblclick(function() {
-                    $inputMessage.val('@' + data.from + ': ' + $inputMessage.val()).focus();
-                });
-            } else { 
-                $messageBodyDiv.css('color', 'yellow');
+            } else { //系统发送信息
+                $messageBodyDiv.css('color', 'red');
             }
             $messageDiv.append($usernameDiv, $messageBodyDiv);
-        } else { 
+        } else {
             if (data.mode === 'private') { //如果是私聊
                 $messageBodyDiv.css('color', 'blue');
             }
             $messageDiv.append($usernameDiv, $messageBodyDiv).addClass("right");
         }
 
-        if (fade) { 
-            $messageDiv.hide().fadeIn(FADE_TIME); 
+        if (fade) {
+            $messageDiv.hide().fadeIn(FADE_TIME);
         }
         $messages.append($messageDiv);
         $messages[0].scrollTop = $messages[0].scrollHeight; //滚动条滚到最底部
     }
 
+    //清空聊天区域
+    function clearChatMessage() {
+        $(".message").remove();
+    }
 
+    //用户列表增加用户
+    function addUserList(data) {
+        $numUsers.text('本聊天室有: ' + data.numUsers + ' 位用户');
+        var $userListElement = $('<li class = "userList">').text(data.username)
+            .data('username', data.username);
+        $userList.append($userListElement);
+    }
 
+    //用户列表清除用户
+    function removeUserList(data) {
+        $numUsers.text('本聊天室有: ' + data.numUsers + ' 位用户');
+        $('.userList').filter(function(i) {
+            return $(this).data('username') === data.username;
+        }).remove();
+    }
 
     /*改变状态*/
 
-    //增加'XX正在输入',data.from = socketIds
-    function addTypingStatus(data) {
-        data.typing = true;
-        data.mode = 'public'
-        data.msg = '正在输入'
-        addChatMessage(data);
+    //增加'XX正在输入'
+    function addTypingStatus(username) {
+        addChatMessage({
+            mode: 'public',
+            from: username,
+            typing: true,
+            msg: '正在输入...'
+        });
     }
 
     //取消'XX正在输入'
-    function removeTypingStatus(data) {
-        getTypingMessages(data).fadeOut(function() {
+    function removeTypingStatus(username) {
+        getTypingMessages(username).fadeOut(function() {
             $(this).remove();
         });
     }
 
     //获得“xx正在输入”的节点
-    function getTypingMessages(data) {        
-        return $('.typing.message').filter(function(i) {            
-            return $(this).data('username') === data.from;
+    function getTypingMessages(username) {
+        return $('.typing.message').filter(function(i) {
+            return $(this).data('username') === username;
         });
     }
 
-    
+
     //获得用户名颜色
-    function getUsernameColor(username) {        
-        var hash = 7; 
+    function getUsernameColor(username) {
+        var hash = 7;
         for (var i = 0; i < username.length; i++) {
             hash = username.charCodeAt(i) + (hash << 5) - hash;
-        }        
+        }
         var index = Math.abs(hash % COLORS.length);
         return COLORS[index];
     }
@@ -163,18 +184,18 @@ $(function() {
     /*对外输出消息*/
     //发送信息
     function sendMessage() {
-        var message = $inputMessage.val();        
-        message = cleanInput(message);        
+        var message = $inputMessage.val();
+        message = cleanInput(message);
         if (message && connected) {
-            $inputMessage.val(''); 
-           
-            var data = {}; 
+            $inputMessage.val('');
+
+            var data = {};
             data.from = clientName;
-            var targetObj = message.match(/^@([\S]+):/);    
-            if (targetObj) { 
-                data.to = targetObj[1]; 
+            var targetObj = message.match(/^@([\S]+):/);
+            if (targetObj) {
+                data.to = targetObj[1];
                 data.mode = 'private';
-                var idx = targetObj[1].length + 3; 
+                var idx = targetObj[1].length + 3;
                 data.msg = message.slice(idx);
             } else {
                 data.to = null;
@@ -183,113 +204,112 @@ $(function() {
             }
             socket.emit('new message', data);
         }
-    }   
+    }
 
     //发送输入状态
     function updateTyping() {
         var mode = $inputMessage.val().match(/^@([\S]+):/);
-        if (connected && (!mode)) {   //公聊状态才发送输入状态     
-            if (!typing) { 
-                typing = true; 
-                socket.emit('typing'); 
+        if (connected && (!mode)) { //公聊状态才发送输入状态     
+            if (!typing) {
+                typing = true;
+                socket.emit('typing');
             }
-            lastTypingTime = (new Date()).getTime(); 
+            lastTypingTime = (new Date()).getTime();
 
-            setTimeout(function() { 
+            setTimeout(function() {
                 var typingTimer = (new Date()).getTime();
-                var timeDiff = typingTimer - lastTypingTime; 
-                if (timeDiff >= TYPING_TIMER_LENGTH && typing) {                                                      
-                    socket.emit('stop typing'); 
-                    typing = false; 
+                var timeDiff = typingTimer - lastTypingTime;
+                if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+                    socket.emit('stop typing');
+                    typing = false;
                 }
-            }, TYPING_TIMER_LENGTH); 
+            }, TYPING_TIMER_LENGTH);
         }
     }
-    
-    /*用户交互*/
 
+    /*用户交互*/
     // Keyboard 事件
-    $window.keydown(function(event) {        
+    $window.keydown(function(event) {
         if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-            $currentInput.focus(); 
+            $currentInput.focus();
         }
-        
+
         if (event.which === 13) {
-            if (clientName) { 
-                sendMessage(); 
-                socket.emit('stop typing'); 
-                typing = false; 
-            } else { 
-                addUser();
+            if (clientName) {
+                sendMessage();
+                socket.emit('stop typing');
+                typing = false;
+            } else {
+                checkUsername();
             }
         }
     });
 
-    $inputMessage.on('input', function() {        
-        updateTyping(); 
+    $inputMessage.on('input', function() {
+        updateTyping();
     });
 
     // Click 事件
-    
+
     $loginPage.click(function() {
         $currentInput.focus();
     });
-    
+
     $inputMessage.click(function() {
         $inputMessage.focus();
     });
-    
+
+    $clearBtn.click(function() {
+        clearChatMessage();
+    });
+
 
 
     /*状态监听*/
-
     socket.on('username exists', function(data) {
         changeUsername(data.msg);
     });
-    
-    socket.on('login success', function(data) {
+
+    socket.on('login success', function(msg) {
         loginSuccess();
-        connected = true;      
-        data.msg = "– 欢迎来到imOline –";
-        addSysInfo(data);
+        connected = true;
+        addSysInfo(msg);
     });
-    
+
     socket.on('new message', function(data) {
         addChatMessage(data);
     });
-    
-    socket.on('user joined', function(data) {
-        data.msg = data.username + ' 已加入!';
-        addSysInfo(data);
+
+    socket.on('user joined', function(username) {
+        addSysInfo(username + ' 已加入!');
     });
-    
-    socket.on('user left', function(data) {
-        log(data.username + ' 已离开!');
-        addSysInfo(data);
-        removeTypingStatus(data);
+
+    socket.on('user left', function(username) {
+        addSysInfo(username + ' 已离开!');
+        removeTypingStatus(username);
     });
-    
-    socket.on('typing', function(data) {
-        addTypingStatus(data);        
+
+    socket.on('typing', function(username) {
+        addTypingStatus(username);
     });
-    
-    socket.on('stop typing', function(data) {
-        removeTypingStatus(data);
+
+    socket.on('stop typing', function(username) {
+        removeTypingStatus(username);
     });
 
     socket.on('disconnect', function() {
-        log('您已失去连接');
+        addSysInfo('您已失去连接');
     });
 
     socket.on('reconnect', function() {
-        log('您已经重新连接');
+        addSysInfo('您已经重新连接');
         if (clientName) {
-            socket.emit('add user', clientName);
+            socket.emit('check username', clientName);
         }
     });
 
     socket.on('reconnect_error', function() {
-        log('尝试重连失败');
+        addSysInfo('尝试重连失败');
     });
 
 });
--- index.js
+++ index.js
@@ -1,92 +1,84 @@
 /*服务器端程序*/
 
 //创建HTTP服务器
-var express = require('express'); 
-var app = express(); 
-var server = app.listen(3000); 
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/app'));
 
 //Chatroom
-var numUsers = 0; 
+var numUsers = 0;
 var unames = {};
-var usockets = {}; 
+var usockets = {};
 
 io.on("connection", function(socket) {
     var addedUser = false;
-    var socketId = '';   
-    
+    var socketId = '';
+
     //sysInfo data数据结构(根据时间不同，可以选择不同属性值)
     //msg: 需要显示的系统提示内容
     //numUsers: 当前聊天室的用户数 
     //username: 系统消息关联的用户名
-    socket.on('add user', function(username) {
+    socket.on('check username', function(username) {
         if (addedUser) return;
         if (username in unames) {
-            socket.emit('username exists', {            	
-            	msg: '昵称已存在,请更换其它昵称'
+            socket.emit('username exists', {
+                msg: '昵称已存在,请更换其它昵称'
             });
             return;
         }
         socketId = username;
         unames[username] = username;
-        usockets[username] = socket; 
-        ++numUsers; 
+        usockets[username] = socket;
+        ++numUsers;
         addedUser = true;
 
-        socket.emit('login success', {        	
-            numUsers: numUsers
-        });
-       
-        socket.broadcast.emit('user joined', {        	
-            username: socketId,
-            numUsers: numUsers,
-        });
+        socket.emit('login success', '欢迎来到imOline聊天室');
 
-        io.sockets.emit('add userList', {  //增加用户列表
-        	username: socketId,
-        	numUsers: numUsers
+        socket.broadcast.emit('user joined', socketId);
+
+        io.sockets.emit('add userList', { //增加用户列表
+            username: socketId,
+            numUsers: numUsers
         });
     });
 
     socket.on('disconnect', function() {
-        if (addedUser) { 
+        if (addedUser) {
             --numUsers;
             addedUser = false;
             delete unames[socketId];
-            delete usockets[socketId];            
-            socket.broadcast.emit('user left'，'remove userList', {
+            delete usockets[socketId];
+            socket.broadcast.emit('user left', socketId);
+            socket.broadcast.emit('remove userList', {
                 username: socketId,
                 numUsers: numUsers,
-            });            
+            });
         }
     });
 
-    //聊天区域data数据结构
+    socket.on('typing', function() {
+        socket.broadcast.emit('typing', socketId);
+    });
+
+    socket.on('stop typing', function() {
+        socket.broadcast.emit('stop typing', socketId);
+    });
+
+    //聊天内容数据结构
     //method: 消息类型，public/private/system
     //from: 消息来源
     //to: 消息目的
     //msg: 消息内容
-    socket.on('typing', function() {
-        socket.broadcast.emit('typing', {        	   
-            from: socketId
-        });
-    });
-    
-    socket.on('stop typing', function() {
-        socket.broadcast.emit('stop typing', {
-            from: socketId
-        });
-    });
-
     socket.on('new message', function(data) {
-        if (!data.to) { 
+        if (!data.to) {
             io.sockets.emit('new message', data);
-        } else if (data.to in unames) {    
+        } else if (data.to in unames) {
             usockets[data.to].emit('new message', data);
-            usockets[data.from].emit('new message', data); 
-        } else {            
+            usockets[data.from].emit('new message', data);
+        } else {
             usockets[data.from].emit('new message', {
                 mode: 'system',
                 from: '系统',
@@ -94,5 +86,5 @@ io.on("connection", function(socket) {
                 msg: '私聊对象已离开!'
             });
         }
-    }); 
+    });
 });
